404725794125be1b1579b377f9d78e943e3d5739
Hernanda Achmad <115292777+hernanda16@users.noreply.github.com>
2023-03-16 20:29:37 +0700

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/her/Documents/control_joy/src
-# Build directory: /home/her/Documents/control_joy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("iris_joystick")
--- build/iris_joystick/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/her/Documents/control_joy/src/iris_joystick
-# Build directory: /home/her/Documents/control_joy/build/iris_joystick
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
